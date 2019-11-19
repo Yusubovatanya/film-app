b17c5b9cbdcb40e311795599eb8c119509929d49
Tanya <tanyagotovets@gmail.com>
2019-11-19 13:57:17 +0200

    test makeFavoriteService

--- src/app/shared/service/user.service.mock.ts
+++ src/app/shared/service/user.service.mock.ts
@@ -3,7 +3,7 @@ import { Film } from "../models/film.model";
 
 export class UserMockService {
   currentFavoritePage = 54;
-  film:Film = {
+  film: Film = {
     poster_path: '/skvI4rYFrKXS73BJxWGH54Omlvv.jpg',
     title: 'testTitleFilm',
     overview: 'test info about film',
@@ -17,19 +17,53 @@ export class UserMockService {
       { iso_3166_1: "US", name: "United States of America" }
     ],
     genres: [
-      { id: 14, name: "фэнтези" }, 
+      { id: 14, name: "фэнтези" },
       { id: 12, name: "приключения" },
     ],
     runtime: 120,
   };
-  filmFavoriteListService: Film[] = [this.film];
-  filmBookMarkListService: Film[] = [this.film];
-  
+  filmSecond: Film = {
+    poster_path: '/skvI4rYFrKXS73BJxWGH54Omlvv.jpg',
+    title: 'testTitleFilm',
+    overview: 'test info about film',
+    release_date: '2019-10-16',
+    popularity: 100,
+    id: 420801,
+    isFavorite: true,
+    isMark: false,
+    vote_average: 5.6,
+    production_countries: [
+      { iso_3166_1: "US", name: "United States of America" }
+    ],
+    genres: [
+      { id: 14, name: "фэнтези" },
+      { id: 12, name: "приключения" },
+    ],
+    runtime: 120,
+  };
+  filmFavoriteListService: Film[] = [this.film, this.filmSecond];
+  filmBookMarkListService: Film[] = [this.film, this.filmSecond];
+
   buildFavorites() {
     return this.filmFavoriteListService.map(film => film.id);
   }
 
   buildMarks() {
     return this.filmBookMarkListService.map(film => film.id);
+  }
+
+  makeFavoriteService(id, status) {
+    const found = this.filmBookMarkListService.map(film => {
+      if (film.id === id) {
+        film.isFavorite = status;
+        return film;
+      }
+    });
+    
+    return found[0];
+  }
+
+  getFavoriteFilmsService() {
+    return this.filmFavoriteListService;
   }
 }
